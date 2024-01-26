0f6787e4b4bf5c15c7efe604b83769ca5cf41d94
raghadhawat <hawatraghad123@gmail.com>
2024-01-26 16:01:11 +0300

    add custom book item

---
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -0,0 +1,27 @@
+import 'package:bookly_app/core/utlis/assets.dart';
+import 'package:flutter/material.dart';
+
+class CustomBookItem extends StatelessWidget {
+  const CustomBookItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .3,
+      child: AspectRatio(
+        aspectRatio: 2.3 / 4,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            image: const DecorationImage(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                AssetsData.testImage,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_item.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -9,6 +10,7 @@ class HomeViewBody extends StatelessWidget {
     return const Column(
       children: [
         CustomAppBar(),
+        CustomBookItem(),
       ],
     );
   }
