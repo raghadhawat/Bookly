5a346e5777bd872804d73baac452d59679fcba3f
raghadhawat <hawatraghad123@gmail.com>
2024-02-09 22:04:39 +0300

    create book details view

---
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BookDetailsViewbody extends StatelessWidget {
+  const BookDetailsViewbody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
