797579cb0a7da6c83b5155b8ed42a56de1d113d8
raghadhawat <hawatraghad123@gmail.com>
2024-02-16 19:38:40 +0300

    implement fetch newest book

--- lib/core/utlis/api_server.dart
+++ lib/core/utlis/api_server.dart
@@ -3,19 +3,18 @@ import 'package:flutter/material.dart';
 
 class ApiServer {
   final _baseUrl = 'https://www.googleapis.com/books/v1/';
-  final Dio dio;
-  ApiServer(this.dio);
+  final Dio _dio;
+  ApiServer(this._dio);
 
   Future<Map<String, dynamic>> get({
     required String endPoint,
-    @required String? token,
+    String? token,
   }) async {
     Map<String, String> headers = {};
     if (token != null) {
       headers.addAll({'Authorization': 'Bearer $token'});
     }
-
-    var response = await dio.get(
+    var response = await _dio.get(
       '$_baseUrl$endPoint',
       options: Options(
         headers: headers,
@@ -33,7 +32,7 @@ class ApiServer {
     if (token != null) {
       headers.addAll({'Authorization': 'Bearer $token'});
     }
-    var response = await dio.post(
+    var response = await _dio.post(
       '$_baseUrl$endPoint',
       data: body,
       options: Options(
@@ -54,7 +53,7 @@ class ApiServer {
     if (token != null) {
       headers.addAll({'Authorization': 'Bearer $token'});
     }
-    var response = await dio.put(
+    var response = await _dio.put(
       '$_baseUrl$endPoint',
       data: body,
       options: Options(
