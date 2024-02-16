dfb9affeef426efdd2aaf0cf794dae77b59a49d8
raghadhawat <hawatraghad123@gmail.com>
2024-02-16 21:20:02 +0300

    create newest states

---
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit() : super(NewestBooksInitial());
+}
